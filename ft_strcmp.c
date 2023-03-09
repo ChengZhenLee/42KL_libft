3211f53f615ddce63709565de0c7c770ffe239e4
ChengZhenLee <zhensstories@gmail.com>
2023-03-09 21:31:39 +0800

    created ft_strcmp.c ft_strncmp.c

---
+++ ft_strcmp.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int	strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = -1;
+	while (s1[++i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
---
+++ ft_strncmp.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int	strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = -1;
+	while ((s1[++i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
