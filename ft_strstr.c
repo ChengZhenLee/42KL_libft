a01e6c281bdb0b0aac19ce955205ca3b36c1e9c5
ChengZhenLee <zhensstories@gmail.com>
2023-03-09 21:21:52 +0800

    ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char	*strstr(const char *haystack, const char *needle)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = -1;
+	if (!needle || !needle[0])
+		return (haystack);
+	if (!haystack)
+		return (NULL);
+	while (haystack[++i])
+	{
+		j = 0;
+		k = i;
+		while (haystack[k] == needle[j])
+		{
+			k++;
+			j++;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
