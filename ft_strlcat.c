0a65d9a34a5f730314505a30cfb939735832d30b
ChengZhenLee <zhensstories@gmail.com>
2023-03-09 20:54:47 +0800

    created ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+size_t	strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dest || !src)
+		return (0);
+	while (dest[i])
+		i++;
+	while (i < size - 1)
+		dest[i++] = src[i];
+	dest[i] = '\0';
+	return (i);
+}
