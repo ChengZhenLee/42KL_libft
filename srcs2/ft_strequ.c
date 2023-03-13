d2743e5fba252561228fba28025e505026b1e2d7
ChengZhenLee <zhensstories@gmail.com>
2023-03-13 20:11:39 +0800

    created ft_strequ.c

---
+++ srcs2/ft_strequ.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int	ft_strequ(char const *s1, char const *s2)
+{
+	int	i;
+
+	i = -1;
+	while (s1[++i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
