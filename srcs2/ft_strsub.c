189cb38643a803fcc05ea6e2b8332ab12b65aef9
ChengZhenLee <zhensstories@gmail.com>
2023-03-13 20:23:22 +0800

    ft_strsub.c

--- srcs2/ft_strsub.c
+++ srcs2/ft_strsub.c
@@ -2,4 +2,18 @@
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
+	size_t	i;
+	char	*res;
 
+	i = start;
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	while (i - start < len)
+	{
+		res[i - start] = s[i];
+		i++;
+	}
+	res[len] = '\0';
+	return (res);
+}
